24c2a7c93cc8d10044919ed7f727e553420ec015
erkha <jmtatout@gmail.com>
2019-11-27 12:26:10 +0100

    quete 7: relation manyToOne

--- src/Migrations/Version20191127110353.php
+++ src/Migrations/Version20191127110353.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191127100005 extends AbstractMigration
+final class Version20191127110353 extends AbstractMigration
 {
     public function getDescription() : string
     {
